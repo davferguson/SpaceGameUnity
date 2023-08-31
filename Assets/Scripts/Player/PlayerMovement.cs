b651ddc0b61e36e322cd505a0c5ea306c19f739b
DaredFTW <davferg43@gmail.com>
2023-08-31 02:12:06 -0400

    added playermovement with left and right animations

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector2 MovementSpeed = new Vector2(100.0f, 100.0f); // 2D Movement speed to have independant axis speed
+    private new Rigidbody2D rigidbody2D; // Local rigidbody variable to hold a reference to the attached Rigidbody2D component
+    private Animator animator;
+    private Vector2 inputVector = new Vector2(0.0f, 0.0f);
+
+    void Awake()
+    {
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        animator.SetFloat("Horizontal", inputVector.x);
+        animator.SetFloat("Speed", inputVector.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + (inputVector * MovementSpeed * Time.fixedDeltaTime));
+    }
+}
