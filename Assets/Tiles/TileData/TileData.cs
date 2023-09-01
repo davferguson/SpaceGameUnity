7b0b5d9644ed8a18d179516e519972cfe6d07d0a
DaredFTW <davferg43@gmail.com>
2023-08-31 21:28:41 -0400

    made tile data inherit RuleTile. small changes to tileset

---
+++ Assets/Tiles/TileData/TileData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Tiles/TileData")]
+public class TileData : RuleTile
+{
+    public Item item;
+    public TileBase[] tiles;
+    public String contents;
+}
